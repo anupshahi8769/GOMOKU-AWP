ccf362302613c7954de5d0ec5a4467a9d7f369b5
Anup Shahi <ashahi@myune.edu.au>
2023-09-20 13:09:43 +1000

    Middleware added to the project

---
+++ gomoku-service/src/middleware/deserializeUser.ts
@@ -0,0 +1,47 @@
+import { NextFunction, Request, Response } from "express";
+import { getUserByID } from '../service/validation.service'
+import { verifyJWT } from '../utils/jwt'
+
+interface TokenBody{
+    username: string;
+    _id: string;
+    iat: number;
+    exp: number;
+}
+
+export const userDeserialize = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try{
+        //getting token
+        let token;
+        if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+            token = req.headers.authorization.split(' ')[1];
+        }
+
+        if(!token){
+            return res.status(403).send("Token is Missing");
+        }
+
+        //validating access token
+        const decode = verifyJWT<TokenBody> (token);
+
+        if(!decode){
+            return res.status(401).send("Invalid Token");
+        }
+
+        const user = await getUserByID(decode._id);
+        
+        if(!user){
+            return res.status(401).send("Invalid User");
+        }
+
+        //sending userid into request
+        req.userId = user._id;
+        next();
+    } catch(err: any){
+        next(err);
+    }
+}
