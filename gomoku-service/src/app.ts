12c9d23fd8e705f774ac7304b97f680552fb84f5
Anup Shahi <ashahi@myune.edu.au>
2023-09-20 12:58:55 +1000

    app.ts coded for connection between server and database, MongoDB will be used for the connection and message will be displayed at console log once the connection is sucessful and running

---
+++ gomoku-service/src/app.ts
@@ -0,0 +1,22 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import connectDB from "./utils/connectDB";
+
+dotenv.config();
+//  Connect to database.
+connectDB();
+const app: Express = express();
+const port = process.env.PORT;
+app.use(express.json());
+
+// app.get('/', (req: Request, res: Response)=>{
+//     res.send('Hello World!');
+// });
+
+mongoose.connection.once("connected", () => {
+  console.log(`⚡️[server]: Connected to MongoDB.`);
+  app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+  });
+});
